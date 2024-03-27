17383d230e6ae718615959e537ff89cb2253c21c
ElisaMavilia <elisamavilia1@gmail.com>
2024-03-27 16:59:21 +0100

    binded multiple attributes to div

--- js/script.js
+++ js/script.js
@@ -7,6 +7,10 @@ const { createApp } = Vue
         text: '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, velit soluta voluptate quaerat vitae magni numquam quam quasi minus quisquam dolor delectus, ipsam incidunt aliquam, hic error optio id ullam?</p>',
         image: "kungalv-ny-3.jpeg",
         imgclass: "picture",
+        obj: {
+            id: "container",
+            class: "text"
+        }
       }
     },
   }).mount('#app')
